55adc6a5a8cf6e90e92a3926602574ada7e70660
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-11 17:46:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array.push("arrays!")
-  puts array.inspect
+  array.element.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
