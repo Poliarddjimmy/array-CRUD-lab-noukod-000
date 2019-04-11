f9b1bb34e954e6927b416093d6cd01615a4fe59b
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-11 17:48:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,11 +8,12 @@ end
 
 def add_element_to_end_of_array(array, element)
   element = "arrays!"
-  array.unshift(element)
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  element = "arrays!"
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
